9ac287da983d67c0692b1500d2231a4d2dc39d3f
topherpaung <christopherpm24@gmail.com>
2025-05-18 15:48:40 +0700

    Update adscontroller.php

--- app/Http/Controllers/adscontroller.php
+++ app/Http/Controllers/adscontroller.php
@@ -8,22 +8,54 @@ class adscontroller extends Controller
 {
     //
     function createads(){
-        
+        $req->validate([
+            'url'=>'required',
+            'description'=>'required'
+        ]);
+        $ad=new Ad;
+        $ad->url=$req->url;
+        $ad->description=$req->description;
+        $ad->save();
     }
     function readads(){
-
+        $ads=Ad::all();
+        return view('ads', ['ads'=>$ads]);
     }
     function queueads(){
-
+        $ads=Ad::where('status', 'queued')->get();
+        return view('ads', ['ads'=>$ads]);
     }
     function modifyads(){
-
+        $req->validate([
+            'id'=>'required',
+            'url'=>'required',
+            'description'=>'required'
+        ]);
+        $ad=Ad::find($req->id);
+        if($ad){
+            $ad->url=$req->url;
+            $ad->description=$req->description;
+            $ad->save();
+        }
+        else{
+            return redirect('/ads',['error1','Ad not found']);
+        }
     }
     function approveads(){
-
+        $req->validate([
+            'id'=>'required'
+        ]);
+        $ad=Ad::find($req->id);
+        if($ad){
+            $ad->status='approved';
+            $ad->save();
+        }
+        else{
+            return redirect('/ads',['error1','Ad not found']);
+        }
     }
     function displayads(){
-
+        $ads=Ad::where('status', 'approved')->get();
     }
 
 }
