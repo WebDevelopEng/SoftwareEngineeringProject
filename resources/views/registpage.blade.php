4aab71ad474670fb9ccb14e9f60daedb7bcd5627
DionisiusAvel <114296203+DionisiusAvel@users.noreply.github.com>
2025-06-15 02:40:41 +0700

    Update registpage.blade.php
    Update regist page terbaru

--- resources/views/registpage.blade.php
+++ resources/views/registpage.blade.php
@@ -4,37 +4,55 @@ Registration
 @endsection
 @section('content')
 <script src="{{asset('viewjs/registration.js')}}"></script>
-<div style="margin:auto;width:80%;text-align:left;margin-top:2%; border-style:solid; border-width:1px;padding:5%;">
-    <h3>Registration</h3>
-    @if ($errors->any())
-    <div class="alert alert-danger">
-        <ul>
-            @foreach ($errors->all() as $error)
-                <li>{{ $error }}</li>
-            @endforeach
-        </ul>
-    </div>
-@endif
-    <form method="post" action="{{route('registacc')}}" id="form">
+<link href="{{asset('viewcss/login.css')}}" rel="stylesheet">
+<link href="//netdna.bootstrapcdn.com/font-awesome/3.2.1/css/font-awesome.css" rel="stylesheet">
+<body style="background-color: rgba(255,235,205,1)">
+<div class="login-card">
+    <form method="post" action="{{route('registacc')}}" id="form"  class="login-form">
         @csrf
-    <button type="button" class="btn btn-primary" style="width:100px" onclick="registtype('userselection')" id="userselection" >User</button>
-<button type="button" class="btn btn-secondary" style="width:100px" onclick="registtype('restoselection')" id="restoselection" >Restaurant</button><br>
-    <label for="username">Enter your username:</label><br>
-    <input type="text" class="form-control" id="username" name="name"> <br>
-    <label for="email"> Enter your email:</label><br>
-    <input type="email" class="form-control" id="email" name="email"><br>
-    <label for="password"  id="password"> Enter your password:</label><br>
-    <input type="password" class="form-control" id="password" name="password"><br>
-    <div id="userselectionarea"> 
-    <label for="dob"> Enter your date of birth:</label><br>
-    <input type="date" class="form-control" id="dob" name="dob"><br><br>
-</div>
-    <div id="restoselectionarea" style="display:none">
-    <label for="location"> Enter your location: </label><br>
-    <textarea id="location" name="location" row="4" column="50"></textarea><br><br>
-</div>
+        <h3 class="login-title" style="margin:0">Welcome to DonaCook!</h3>
+        @if ($errors->any())
+            <div class="alert alert-danger">
+                <ul>
+                    @foreach ($errors->all() as $error)
+                        <li>{{ $error }}</li>
+                    @endforeach
+                </ul>
+            </div>
+        @endif
+        <div class="login-tabs">
+            <button type="button" class="login-tab" onclick="registtype('userselection')" id="userselection" >User</button>
+            <button type="button" class="login-tab" onclick="registtype('restoselection')" id="restoselection" >Restaurant</button>
+        </div>
+        <div class="form-group">
+            <label for="username">Enter your username:</label>
+            <input type="text" class="form-control" id="username" name="name">
+        </div>
+        <div class="form-group">
+            <label for="email"> Enter your email:</label>
+            <input type="email" class="form-control" id="email" name="email">
+        </div>
+        <div class="form-group">
+            <label for="password"  id="password"> Enter your password:</label>
+            <input type="password" class="form-control" id="password" name="password">
+        </div>
+        <div class="form-group">
+            <div id="userselectionarea"> 
+            <label for="dob"> Enter your date of birth:</label>
+            <input type="date" class="form-control" id="dob" name="dob">
+        </div>
 
-    <input class="btn btn-success" type="submit" id="submit" name="submit"><br>
-</form>
+        <div class="form-group">
+            <div id="restoselectionarea" style="display:none">
+            <label for="location"> Enter your location: </label>
+            <textarea id="location" name="location" row="4" column="50"></textarea>
+            </div>
+        </div>
+
+        <input type="text" style="display:none" name="hidden" id="hidden" value="userselection">
+        <button type="submit" class="btn-submit">Register</button>
+    </form>
 </div>
+</div>
+</body>
 @endsection
