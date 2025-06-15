f2bea3931f55eb4828dfb8910fb0ca2091efc403
DionisiusAvel <114296203+DionisiusAvel@users.noreply.github.com>
2025-06-16 04:01:16 +0700

    Update addashboard.blade
    Design buat ad dashboard, dan css buat ad dashboard

--- resources/views/addashboard.blade.php
+++ resources/views/addashboard.blade.php
@@ -1,65 +1,86 @@
 @extends('templates.headandfoot')
-@section('title')
-Ad Management
-@endsection
+
+@section('title', 'Ad Management')
+
 @section('content')
-<h3 style="padding-left:5%;padding-top:1%;padding-bottom:1%;">Ad Management</h3>
-<div style="width:80%;margin:auto;display:flex;">
-<div style="width:60%;">
-@if(isset($ads))
-@foreach($ads as $ad)
-@php
-    $imagestring="/storage/advertimages/".$ad->image;
-@endphp
-<div style="border-style:solid;border-color:black;border-width:1px;">
-<div style="width:20%;display:inline-block;">
-@if($ad->image !==null)
-<img src="{{$imagestring}}"  style="object-fit:cover;height:100px;width:100%;">
-@endif
-</div>
-<div style="width:50%;height:100%;display:inline-block;">
-<b>Title:</b>
-{{$ad->title}}<br>
-<b>Description:</b>
-    {{Str::limit($ad->description,50)}}
-</div>
-<a class="btn btn-info" href="{{route('editad',['id'=>$ad->id])}}">Edit</a>
-<a class="btn btn-warning" href="{{route('deletead',['id'=>$ad->id])}}">Delete</a>
-</div>
-@endforeach
+<link href="{{ asset('viewcss/addashboard.css') }}" rel="stylesheet">
 
-@endif
+<body style="background-color: blanchedalmond">
+    <div class="ad-card2">
+        <h3 class="ad-title">Ad Management</h3>
+        <div class="row gx-4">
+            {{-- View Ads --}}
+            <div class="col-md-7 mb-4">
+                @if(isset($ads))
+                    @foreach($ads as $ad)
+                        @php $imagestring = "/storage/advertimages/" . $ad->image; @endphp
+                        <div class="ad-card p-3 mb-3 d-flex align-items-center justify-content-between">
+                            <div class="d-flex" style="width: 100%;">
+                                @if($ad->image)
+                                <img src="{{ $imagestring }}" class="img-thumbnail me-3" style="object-fit:cover;height:100px;width:120px;border-radius:8px;">
+                                @endif
+                                <div class="flex-grow-1 title-desc">
+                                    <strong>Title:</strong> {{ $ad->title }} <br>
+                                    <strong>Description:</strong> {{ Str::limit($ad->description, 50) }}
+                                </div>
+                                <div class="d-flex flex-column justify-content-center gap-2 ms-3">
+                                    <a href="{{ route('editad', ['id' => $ad->id]) }}" class="btn-submit small-btn">Edit</a>
+                                </div>
+                                <div class="d-flex flex-column justify-content-center gap-2 ms-3">
+                                    <a href="{{ route('deletead', ['id' => $ad->id]) }}" class="btn-submit small-btn danger">Delete</a>
+                                </div>
+                            </div>
+                        </div>
+                    @endforeach
+                @endif
 
-</div>
-<div style="width:40%;border-style:solid;border-color:black;border-width:1px;padding-top:1%;padding-bottom:1%;">
-<div style="width:80%;margin:auto;">
-<h5>Create New Ad</h5>
-@if ($errors->any())
-    <div class="alert alert-danger">
-        <ul>
-            @foreach ($errors->all() as $error)
-                <li>{{ $error }}</li>
-            @endforeach
-        </ul>
+                {{-- Pagination --}}
+                <div class="pagination-wrapper mt-3">
+                    {{ $ads->links() }}
+                </div>
+            </div>
+
+            {{-- Create Ad Form --}}
+            <div class="col-md-5 mb-4">
+                <div class="create-card p-4">
+                    <h5 class="ad-title">Create New Ad</h5>
+                    @if ($errors->any())
+                        <div class="alert alert-danger mt-3">
+                            <ul class="mb-0">
+                                @foreach ($errors->all() as $error)
+                                    <li>{{ $error }}</li>
+                                @endforeach
+                            </ul>
+                        </div>
+                    @endif
+
+                    <form method="POST" action="{{ route('createads') }}" enctype="multipart/form-data" class="ad-form mt-3">
+                        @csrf
+                        <div class="form-group">
+                            <label for="title">Title:</label>
+                            <input type="text" class="form-control" name="title" id="title">
+                        </div>
+
+                        <div class="form-group">
+                            <label for="description">Description:</label>
+                            <textarea name="description" id="description"></textarea>
+                        </div>
+
+                        <div class="form-group">
+                            <label for="image">Image:</label>
+                            <input type="file" class="form-control" name="image" id="image">
+                        </div>
+
+                        <button type="submit" class="btn-submit mt-2">Create Ad</button>
+                    </form>
+                </div>
+            </div>
+        </div>
     </div>
-@endif
-<form method="post" action="{{route('createads')}}" enctype="multipart/form-data">
-@csrf
-<label for="title">Title:</label>
-<input class="form-control" type="text" name="title" id="title">
-<label for="description">Description:</label>
-<textarea class="form-control" name="description" id="description" ></textarea>
-<label for="image">Image: </label>
-<input class="form-control" type="file" name="image" id="image">
-<br>
-<button class="btn btn-dark" type="submit">Create Ad</button>
-
-</form>
-</div>
-</div>
-</div>
-<div style="width:fit-content;margin:auto;" >
-{{$ads->links()}}
-</div>
-
+</body>
 @endsection
+
+
+
+
+
